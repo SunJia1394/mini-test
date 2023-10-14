b3dbc00815db9257cbfcd200f9d2323df8694fb4
sunrujia <94sunjia@gmail.com>
2023-10-15 06:00:39 +0800

    修改代码

--- miniprogram/pages/logs/logs.ts
+++ miniprogram/pages/logs/logs.ts
@@ -1,40 +1,28 @@
 // logs.ts
 // const util = require('../../utils/util.js')
-import { formatTime } from '../../utils/util'
+// import * as qiniuUploader from '../../utils/qiniuUploader';
 
-// Page({
-//   data: {
-//     logs: [],
-//   },
-//   onLoad() {
-//     this.setData({
-//       logs: (wx.getStorageSync('logs') || []).map((log: string) => {
-//         return {
-//           date: formatTime(new Date(log)),
-//           timeStamp: log
-//         }
-//       }),
-//     })
-//   },
-// })
+// function initQiniu() {
+//   const options: qiniuUploader.QiniuOptions = {
+//     region: 'ECN',   // 华东
+//     uptokenURL: 'YOUR_SERVER_API',  // 你的服务器 API，用于获取临时的 uptoken
+//     domain: 'image1.juramaia.com',  // 文件上传后，访问该文件的域名
+//     shouldUseQiniuFileName: false  // 是否使用七牛云上的文件名。为 false 时，使用微信自动生成的文件名
+//   };
+//   qiniuUploader.init(options);
+// }
+const baseurl = 'https://b237-2409-891e-3520-25cc-51be-1e2a-fed7-4efa.ngrok-free.app'
+const token = "Bearer 23bdc938023098ef06cc489fb79888927a382533433368757012784fbc42368b2f8ec81a1f1bf53ed293426d1e95dafee4242a567d8fa6b32f7bf9b7bc52e4aabca273cab2b7ee468839fa750eb276aa041ed602d8f1b2f25ae982298a4095245a65928bb4e3aee461bd7b787fe5686b4a3df963c803368e4c79555bbdbd14bc"
 
 Page({
   data: {
     data: [{
       avatarPath: 'https://th.bing.com/th?id=OIP.RoNHAE_HE9e8rOdgDDPoPgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2', // 需要你自己填充数据
-      avatarName: 'test',
-      brief: 'dfd',
+      avatarName: '奥比',
       imageUrl: 'https://th.bing.com/th?id=OIP.RoNHAE_HE9e8rOdgDDPoPgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2',
       comments: 0,
       likes: 0,
-    }, {
-      avatarPath: 'https://th.bing.com/th?id=OIP.RoNHAE_HE9e8rOdgDDPoPgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2', // 需要你自己填充数据
-      avatarName: 'test',
-      brief: 'dfd',
-      imageUrl: 'https://th.bing.com/th?id=OIP.RoNHAE_HE9e8rOdgDDPoPgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2',
-      comments: 0,
-      likes: 0,
-    },],
+    }],
     fileList: [
     ],
   },
@@ -55,37 +43,96 @@ Page({
   },
   // https://tamemev-100-tobiashoffman.replit.app/main_process
   afterRead(event: { detail: { file: any; }; }) {
+    let that = this;
     const { file } = event.detail;
-    console.log(file)
+    console.log(file);
+    wx.uploadFile({
+      url: 'https://b237-2409-891e-3520-25cc-51be-1e2a-fed7-4efa.ngrok-free.app/api/upload',  // 你的上传 API 地址
+      filePath: file.tempFilePath,  // 需要上传的文件的路径
+      name: 'files',  // 文件对应的 key , 开发者在服务器端通过这个 key 可以获取到文件二进制内容
+      header: {
+        'Authorization': token
+      },
+      success(res) {
+        console.log('上传成功！', res.data)
+        let jsonObject = JSON.parse(res.data);
+        console.log(jsonObject[0].url)
+        const imageUrl = `${baseurl}${jsonObject[0].url}`
+        getAnalyze(imageUrl);
+      },
+      fail(err) {
+        console.log('上传失败：', err)
+      }
+    })
 
-    // wx.request({
-    //   url: 'https://tamemev-100-tobiashoffman.replit.app/analyze_process', // 更改为你的实际接口地址
-    //   method: 'POST',
-    //   data: {
-    //     text: file.url, // 这里填写你需要发送的字符串
-    //   },
-    //   header: {
-    //     'content-type': 'application/json' 
-    //   },
-    //   success(res) {
-    //     console.log(res.data);
-    //   },
-    //   fail(error) {
-    //     console.error("上传失败:", error);
-    //   },
-    // });
+    console.log(file.tempFilePath)
 
-    // wx.uploadFile({
-    //   url: 'https://example.weixin.qq.com/upload/main_process', // 仅为示例，非真实的接口地址
-    //   filePath: file.url,
-    //   name: 'file',
-    //   formData: { user: 'test' },
-    //   success(res) {
-    //     // 上传完成需要更新 fileList
-    //     const { fileList = [] } = this.data;
-    //     fileList.push({ ...file, url: res.data });
-    //     this.setData({ fileList });
-    //   },
-    // });
+
+
+    const getAnalyze = (imageUrl: string) => {
+      wx.request({
+        url: 'https://tamemev-100-tobiashoffman.replit.app/api/v1/analyze', // 更改为你的实际接口地址
+        method: 'POST',
+        data: {
+          text: imageUrl, // 这里填写你需要发送的字符串
+        },
+        timeout: 300000,
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          const data = res.data;
+          // @ts-ignore
+          console.log(res.data.output);
+          setTimeout(() => {
+            // @ts-ignore
+            getGenerate(data.output || "")
+          }, 2000)
+
+        },
+        fail(error) {
+          console.error("创建失败:", error);
+        },
+      });
+    }
+
+    const getGenerate = (analyze: string) => {
+      wx.request({
+        url: 'https://tamemev-100-tobiashoffman.replit.app/api/v1/generate', // 更改为你的实际接口地址
+        method: 'POST',
+        data: {
+          text: analyze, // 这里填写你需要发送的字符串
+        },
+        timeout: 300000,
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          let newArray = that.data.data;
+          // @ts-ignore
+          const imageUrl = res.data.image_url || "";
+          if (imageUrl) {
+            let newItem = {
+              avatarPath: 'https://th.bing.com/th?id=OIP.RoNHAE_HE9e8rOdgDDPoPgHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2',
+              avatarName: 'test',
+              imageUrl: imageUrl,
+              brief: '',
+              comments: 0,
+              likes: 0
+            };
+            newArray.unshift(newItem);
+            that.setData({ data: newArray });
+          }
+
+
+        },
+        fail(error) {
+          console.error("创建失败:", error);
+        },
+      });
+    }
+
+
+
   },
 });
